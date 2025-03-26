e4f0a9677b265fe2b1bac697b862259e85460e30
Yuan <49439486+yuanukim@users.noreply.github.com>
2025-03-26 20:14:35 +0800

    now,  only print distinct line information, and print the total leaks bytes.

--- mem_detect.c
+++ mem_detect.c
@@ -1,12 +1,95 @@
 /*
-	@author yuanukim
+    @author yuanukim
 */
 #include "mem_detect.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define HASH_SET_BUCKET_SIZE  101
+
+typedef struct HashNode {
+    int line;
+    struct HashNode* next;
+} HashNode;
+
+typedef struct HashSet {
+    HashNode* bucket[HASH_SET_BUCKET_SIZE];
+} HashSet;
+
 static MemList memList;
+
+HashSet* hash_set_create(void) {
+    HashSet* hs = (HashSet*)malloc(sizeof(HashSet));
+    
+    for (size_t i = 0; i < HASH_SET_BUCKET_SIZE; ++i) {
+        hs->bucket[i] = NULL;
+    }
+    
+    return hs;
+}
+
+void hash_set_destroy(HashSet* hs) {
+    if (hs) {
+        HashNode* cursor;
+        HashNode* temp;
+        
+        for (size_t i = 0; i < HASH_SET_BUCKET_SIZE; ++i) {
+            cursor = hs->bucket[i];
+            
+            while (cursor) {
+                temp = cursor->next;
+                free(cursor);
+                cursor = temp;
+            }
+        }
+        
+        free(hs);
+    }
+}
+
+int hash(int key) {
+    return key % HASH_SET_BUCKET_SIZE;
+}
+
+HashNode* hash_set_find(HashSet* hs, int key) {
+    int hashval = hash(key);
+    HashNode* node = hs->bucket[hashval];
+    
+    while (node) {
+        if (node->line == key) {
+            return node;
+        }
+        else {
+            node = node->next;
+        }
+    }
+    
+    return NULL;
+}
+
+void hash_set_add(HashSet* hs, int key) {
+    HashNode* node = hash_set_find(hs, key);
+    
+    if (node != NULL) {
+        return;
+    }
+    
+    node = (HashNode*)malloc(sizeof(HashNode));
+    node->line = key;
+    node->next = NULL;
+    
+    int hashval = hash(key);
+    HashNode* cursor = hs->bucket[hashval];
+    
+    if (cursor == NULL) {
+        hs->bucket[hashval] = node;
+    }
+    else {
+        cursor->next = node;
+        cursor = cursor->next;
+    }
+}
 
 void mem_detect_init(void) {
     memList.head = (MemNode*)malloc(sizeof(MemNode));
@@ -34,7 +117,7 @@ void mem_detect_destroy(void) {
 
 void* mem_detect_malloc(size_t size, int line, const char* fileName, const char* functionName) {
     MemNode* node = (MemNode*)malloc(sizeof(MemNode) + size);
-	MemNode* tailPrev = memList.tail->prev;
+    MemNode* tailPrev = memList.tail->prev;
     
     node->size = size;
     node->line = line;
@@ -51,11 +134,11 @@ void* mem_detect_malloc(size_t size, int line, const char* fileName, const char*
 }
 
 void* mem_detect_calloc(size_t nmemb, size_t size, int line, const char* fileName, const char* functionName) {
-	size_t totalSize = nmemb * size;
-	void* memory = mem_detect_malloc(totalSize, line, fileName, functionName);
-	memset(memory, 0, totalSize);
+    size_t totalSize = nmemb * size;
+    void* memory = mem_detect_malloc(totalSize, line, fileName, functionName);
+    memset(memory, 0, totalSize);
     
-	return memory;
+    return memory;
 }
 
 void mem_detect_free(void* mem) {
@@ -69,10 +152,28 @@ void mem_detect_free(void* mem) {
 }
 
 void mem_detect_print_leak(void) {
+    size_t totalLeak = 0;
+    HashSet* hs = hash_set_create();
     MemNode* cursor = memList.head->next;
     
+    printf("\nmemory leak detection finish.\n");
+    
     while (cursor != memList.tail) {
-        printf("[LINE]: %d, [FILE]: %s, [FUNC_NAME]: %s, leak %d bytes.\n", cursor->line, cursor->fileName, cursor->functionName, cursor->size);
+        if (hash_set_find(hs, cursor->line) == NULL) {
+            hash_set_add(hs, cursor->line);
+            printf("[LINE]: %d, [FILE]: %s, [FUNC_NAME]: %s, leak.\n", cursor->line, cursor->fileName, cursor->functionName);
+        }
+        
+        totalLeak += cursor->size;
         cursor = cursor->next;
+    }
+    
+    hash_set_destroy(hs);
+    
+    if (totalLeak > 0) {
+        printf("total leak %d bytes.\n", totalLeak);
+    }
+    else {
+        printf("no leaks.\n");
     }
 }
